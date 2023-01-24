b6bf85f9f6f6e9fc6c164fe0e38c986a31ca298d
CodeSandbox <support@codesandbox.io>
2023-01-24 12:19:13 +0000

    Initial commit

---
+++ build-tokens.js
@@ -0,0 +1,46 @@
+const { registerTransforms } = require("@tokens-studio/sd-transforms");
+const StyleDictionary = require("style-dictionary");
+
+registerTransforms(StyleDictionary);
+
+const sd = StyleDictionary.extend({
+  source: ["tokens/output.json"],
+  platforms: {
+    js: {
+      transformGroup: "tokens-studio",
+      buildPath: "build/js/",
+      files: [
+        {
+          destination: "variables.js",
+          format: "javascript/es6",
+        },
+      ],
+    },
+    css: {
+      transforms: [
+        "ts/descriptionToComment",
+        "ts/size/px",
+        "ts/size/css/letterspacing",
+        "ts/size/lineheight",
+        "ts/type/fontWeight",
+        "ts/resolveMath",
+        "ts/typography/css/shorthand",
+        "ts/border/css/shorthand",
+        "ts/shadow/css/shorthand",
+        "ts/color/css/hexrgba",
+        "ts/color/modifiers",
+        "name/cti/kebab",
+      ],
+      buildPath: "build/css/",
+      files: [
+        {
+          destination: "variables.css",
+          format: "css/variables",
+        },
+      ],
+    },
+  },
+});
+
+sd.cleanAllPlatforms();
+sd.buildAllPlatforms();
