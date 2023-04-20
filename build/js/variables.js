8d8cda3dd3e2c1e665c6759463fb9dae79f31c78
entape <entape@users.noreply.github.com>
2023-04-20 21:42:03 +0000

    Update Tokens

--- build/js/variables.js
+++ build/js/variables.js
@@ -1,31 +1,8 @@
 /**
  * Do not edit directly
- * Generated on Thu, 20 Apr 2023 21:10:02 GMT
+ * Generated on Thu, 20 Apr 2023 21:42:02 GMT
  */
 
-export const buttonPrimaryBackground = "#38a169";
-export const buttonPrimaryText = "#ffffff";
-export const buttonBorderRadius = "16px";
-export const buttonBorderWidth = "8px";
-export const cardBorderRadius = "8px";
-export const cardBackground = "#1a202c";
-export const cardPadding = "16px";
-export const boxShadowDefault = "5px 5px 5px 3px #00000026, inset 4px 4px 5px 6px #00000033";
-export const typographyH1Bold = "700 48.829px/110% Inter";
-export const typographyH1Regular = "400 48.829px/110% Inter";
-export const typographyH2Bold = "700 39.063px/110% Inter";
-export const typographyH2Regular = "400 39.063px/110% Inter";
-export const typographyBody = "400 16px/110% Roboto";
-export const fgDefault = "#ffffff";
-export const fgMuted = "#e2e8f0";
-export const fgSubtle = "#a0aec0";
-export const bgDefault = "#1a202c";
-export const bgMuted = "#4a5568";
-export const bgSubtle = "#718096";
-export const accentDefault = "#38a169";
-export const accentOnAccent = "#ffffff";
-export const accentBg = "#276749";
-export const shadowsDefault = "#0000004d";
 export const dimensionScale = "2px";
 export const dimensionXs = "4px";
 export const dimensionSm = "8px";
@@ -159,7 +136,26 @@ export const fontSizesH6 = "16px";
 export const fontSizesBody = "16px";
 export const fontSizesSm = "13.6px";
 export const fontSizesXs = "10.4px";
-export const tokenSetOrder0 = "core";
-export const tokenSetOrder1 = "light";
-export const tokenSetOrder2 = "dark";
-export const tokenSetOrder3 = "theme";
+export const buttonPrimaryBackground = "#38a169";
+export const buttonPrimaryText = "#ffffff";
+export const buttonBorderRadius = "16px";
+export const buttonBorderWidth = "8px";
+export const cardBorderRadius = "8px";
+export const cardBackground = "#1a202c";
+export const cardPadding = "16px";
+export const boxShadowDefault = "5px 5px 5px 3px #00000026, inset 4px 4px 5px 6px #00000033";
+export const typographyH1Bold = "700 48.829px/110% Inter";
+export const typographyH1Regular = "400 48.829px/110% Inter";
+export const typographyH2Bold = "700 39.063px/110% Inter";
+export const typographyH2Regular = "400 39.063px/110% Inter";
+export const typographyBody = "400 16px/110% Roboto";
+export const fgDefault = "#ffffff";
+export const fgMuted = "#e2e8f0";
+export const fgSubtle = "#a0aec0";
+export const bgDefault = "#1a202c";
+export const bgMuted = "#4a5568";
+export const bgSubtle = "#718096";
+export const accentDefault = "#38a169";
+export const accentOnAccent = "#ffffff";
+export const accentBg = "#276749";
+export const shadowsDefault = "#0000004d";
